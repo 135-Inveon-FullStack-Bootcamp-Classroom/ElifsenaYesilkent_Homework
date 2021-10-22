9552d1f1669fd340bb618c523e461fd3e017ccfa
elfyslknt <elifsenayslknt@gmail.com>
2021-10-22 23:35:01 +0300

    To do list

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-/**
- * 1. todo ekleme
- * 2. tamamla butonu (complete)
- * 3. sil butonu olacak
- * 4. kayıt edilsin (localStorage, cookie) - sayfa yenilendiği zaman son hali yeniden görünsün
- *
- * --- optional
- * 1. güncelleme
- * 2. toplu silme (mehmete required gerisi optional)+++++
- * 3. eklenme - tamamlanma tarihi++++
- * 4. berke -> tamamlanma tarihi gelince alert version----
- * 5. kategoriler koyulabilir -> spor, iş vs.----
- * 6.
- */
 
 const button = document.querySelector("#button-save");
 const text = document.querySelector("#text-input");
@@ -27,7 +13,6 @@ const check = document.querySelector("#check-input");
 let listStr = "";
 
 ulList.innerHTML+=localStorage.getItem("List");
-
 
 function onButtonClick() {
 
