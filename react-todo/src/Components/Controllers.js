5454af7b8bb69d222597df462647b376036b30cc
elfyslknt <elifsenayslknt@gmail.com>
2021-10-28 14:40:59 +0300

    React with todo created

---
+++ react-todo/src/Components/Controllers.js
@@ -0,0 +1,10 @@
+const Controllers=({inputValue,setInputValue})=>(
+
+    <div style={{marginLeft: "100px", height:"80px", marginRight: "150px", marginTop:"20px", backgroundColor :"#D6DBDF", 
+    display: "flex",  justifyContent: "space-evenly"}}>
+        <input  type="text" style={{ margin:"20px",   width: "800px",  height: "30px", fontSize: "25px"}}></input>
+        <button style={{ width: "105px", height: "33px" ,marginTop:"20px"}}>Kaydet</button>
+    </div>
+);
+
+export default Controllers;
---
+++ react-todo/src/Components/List.js
@@ -0,0 +1,10 @@
+const List = () => (
+    <div style={{marginLeft: "100px", marginRight: "150px", marginTop:"10px", backgroundColor :"#FADBD8", 
+    display: "flex", flexDirection: "column", }}>
+        <ul>
+            
+        </ul>
+    </div>
+); 
+
+export default List;
