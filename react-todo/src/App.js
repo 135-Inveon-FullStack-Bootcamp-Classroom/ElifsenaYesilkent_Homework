7fdfc87468a8d7c0c699d798c7be00cf4f15aa22
elfyslknt <elifsenayslknt@gmail.com>
2021-10-28 14:41:32 +0300

    React with todo created

---
+++ react-todo/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Controllers from './Components/Controllers';
+import Header from './Components/Header';
+import List from './Components/List';
+
+function App() {
+  return (
+  <div>
+  <Header /> 
+  <Controllers/> 
+  <List/>
+    </div>
+  );
+
+   
+}
+
+export default App;
---
+++ react-todo/src/Components/Header.js
@@ -0,0 +1,8 @@
+const Header = () =>(
+    <div style={{marginLeft: "100px", marginRight: "150px", marginTop:"30px", backgroundColor :"#85929E", 
+    display: "flex", flexDirection: "row", justifyContent: "center"}}>
+        <h1>TODOS</h1>
+    </div>
+);
+
+export default Header;
