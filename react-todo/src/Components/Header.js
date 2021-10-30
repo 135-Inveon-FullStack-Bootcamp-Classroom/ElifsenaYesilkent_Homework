0fd9914179ab1d1250c7156f39e2143f1d961e53
elfyslknt <elifsenayslknt@gmail.com>
2021-10-30 03:17:53 +0300

    added delete buton

--- react-todo/src/Components/Header.js
+++ react-todo/src/Components/Header.js
@@ -1,7 +1,10 @@
 const Header = () =>(
-    <div style={{marginLeft: "250px", marginRight: "250px", marginTop:"30px", backgroundColor :"#85929E", 
+    <div style={{marginLeft: "250px", marginRight: "250px",
+     marginTop:"30px", backgroundColor :"#85929E", 
     display: "flex", flexDirection: "row", justifyContent: "center"}}>
+        
         <h1>TODOS</h1>
+        
     </div>
 );
 
