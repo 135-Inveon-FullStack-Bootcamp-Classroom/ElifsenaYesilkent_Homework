bd36491d977e2bf4bf1c20b85a1c0adac3f79113
elfyslknt <elifsenayslknt@gmail.com>
2021-11-08 23:21:23 +0300

    update

--- fetch-app/src/App.js
+++ fetch-app/src/App.js
@@ -4,27 +4,44 @@ import {useState,useEffect} from "react";
 
 function App() {
 
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
   const [data,setData]=useState();
-  useEffect(()=>{
-    
-  });
-
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div style={{fontSize:"20px" , display:"flex" , flexDirection: "row", justifyContent: "center"}}>
+        <h1>  COLOR</h1>
+      </div>
+      <br/>
+       <ul style={{listStyleType:"none",  display:"flex", flexDirection:"row",flexWrap:"wrap",justifyContent:"space-around"}}>
+        {items.map(item => (
+          <div style={{padding:"10px"}}>
+            <li style={{paddingTop:"15px", border: "3px solid black", height:"190px", width:"200px"}} key={item.id}>
+              <img src={item.url} style={{width:"100px", height:"100px"}}></img>
+              <br/>
+              <b>{item.title}</b>
+            </li>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
