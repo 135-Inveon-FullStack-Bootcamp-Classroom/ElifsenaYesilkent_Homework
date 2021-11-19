c52c518d9f942c3af585fa63561a9cee8d223de1
elfyslknt <elifsenayslknt@gmail.com>
2021-11-19 16:38:44 +0300

    project created

---
+++ week-5-OOP.Formula1/week-5-OOP.Formula1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace week_5_OOP.Formula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
